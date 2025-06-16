59c6134a86f875a5156cd88ac280903f42d10dba
Kashish708 <kkashish792004@gmail.com>
2025-06-17 00:11:12 +0530

    Update BigInteger Calculator Project.cpp

--- BigInteger Calculator Project.cpp
+++ BigInteger Calculator Project.cpp
@@ -1,5 +1,4 @@
-// C++ program to implement
-// the above approach
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,36 +7,36 @@ class BigInt{
     string digits;
 public:
 
-    //Constructors:
+    
     BigInt(unsigned long long n = 0);
     BigInt(string &);
     BigInt(const char *);
     BigInt(BigInt &);
 
-    //Helper Functions:
+    
     friend void divide_by_2(BigInt &a);
     friend bool Null(const BigInt &);
     friend int Length(const BigInt &);
     int operator[](const int)const;
 
-               /* * * * Operator Overloading * * * */
+               
 
-    //Direct assignment
+    
     BigInt &operator=(const BigInt &);
 
-    //Post/Pre - Incrementation
+    
     BigInt &operator++();
     BigInt operator++(int temp);
     BigInt &operator--();
     BigInt operator--(int temp);
 
-    //Addition and Subtraction
+    
     friend BigInt &operator+=(BigInt &, const BigInt &);
     friend BigInt operator+(const BigInt &, const BigInt &);
     friend BigInt operator-(const BigInt &, const BigInt &);
     friend BigInt &operator-=(BigInt &, const BigInt &);
 
-    //Comparison operators
+    
     friend bool operator==(const BigInt &, const BigInt &);
     friend bool operator!=(const BigInt &, const BigInt &);
 
@@ -46,28 +45,28 @@ public:
     friend bool operator<(const BigInt &, const BigInt &);
     friend bool operator<=(const BigInt &, const BigInt &);
 
-    //Multiplication and Division
+    
     friend BigInt &operator*=(BigInt &, const BigInt &);
     friend BigInt operator*(const BigInt &, const BigInt &);
     friend BigInt &operator/=(BigInt &, const BigInt &);
     friend BigInt operator/(const BigInt &, const BigInt &);
 
-    //Modulo
+    
     friend BigInt operator%(const BigInt &, const BigInt &);
     friend BigInt &operator%=(BigInt &, const BigInt &);
 
-    //Power Function
+    
     friend BigInt &operator^=(BigInt &,const BigInt &);
     friend BigInt operator^(BigInt &, const BigInt &);
     
-    //Square Root Function
+    
     friend BigInt sqrt(BigInt &a);
 
-    //Read and Write
+    
     friend ostream &operator<<(ostream &,const BigInt &);
     friend istream &operator>>(istream &, BigInt &);
 
-    //Others
+    
     friend BigInt NthCatalan(int n);
     friend BigInt NthFibonacci(int n);
     friend BigInt Factorial(int n);
@@ -439,7 +438,7 @@ ostream &operator<<(ostream &out,const BigInt &a){
     return cout;
 }
 
-//Driver code with some examples
+
 int main()
 {
     BigInt first("12345");
@@ -462,14 +461,14 @@ int main()
         cout << "fifth is larger than fourth!\n";
     }
 
-    // Printing all the numbers
+    
     cout << "first = " << first << '\n';
     cout << "second = " << second << '\n';
     cout << "third = " << third << '\n';
     cout << "fourth = " << fourth<< '\n';
     cout << "fifth = " << fifth<< '\n';
 
-    // Incrementing the value of first
+    
     first++;
     cout << "After incrementing the"
          << " value of first is : ";
@@ -483,7 +482,7 @@ int main()
     cout << "Product of second and third = "
          << product << '\n';
 
-    // Print the fibonacci number from 1 to 100
+    
     cout << "-------------------------Fibonacci"
          << "------------------------------\n";
     for (int i = 0; i <= 100; i++) {
@@ -499,7 +498,7 @@ int main()
         cout << "Catalan " << i << " = " << Cat<<'\n';
     }
 
-    // Calculating factorial of from 1 to 100
+    
     cout << "-------------------------Factorial"
          << "------------------------------\n";
     for (int i = 0; i <= 100; i++) {
@@ -509,6 +508,5 @@ int main()
              << i << " = ";
         cout << fact << '\n';
     }
-  // This code is contributed
-  // by Gatea David
+  
 }
